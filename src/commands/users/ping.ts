698d7e949e223bf3ffbb4893f8618c5aacd71859
Jeredy <brunojessie144@gmail.com>
2025-09-08 17:56:46 -0300

    up

--- src/commands/users/ping.ts
+++
@@ -1,7 +0,0 @@
-import { setupMessagingServices } from "../../exports/messages.js";
-
-export async function ping(faputa: any, from: string, m: any) {
-  const { enviarTexto } = setupMessagingServices(faputa, from, m);
-  await enviarTexto("Pong!");
-}
-
