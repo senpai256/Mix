86f1d9f945d9f7b0f300ec9eca66b3b4f1dfb31a
Jeredy <brunojessie144@gmail.com>
2025-09-19 00:10:03 -0300

    barrigas

--- build/commands/users/menu.js
+++ build/commands/users/menu.js
@@ -1,10 +1,11 @@
 import { setupMessagingServices } from "../../exports/messages.js";
+import { menuCaption } from "../handle/index.js";
 //
 export async function menu(faputa, from, m) {
     const { enviarTexto, enviarAudioGravacao, enviarImagem } = setupMessagingServices(faputa, from, m);
     try {
-        await enviarAudioGravacao("assets/audios/mmmp3");
-        await enviarImagem("assets/imgs/neko.png");
+        await enviarAudioGravacao("./db/assets/audio/mm.mp3");
+        await enviarImagem("db/assets/img/neko.png", menuCaption(m));
     }
     catch (error) {
         console.log("errror");
--- src/commands/users/menu.ts
+++ src/commands/users/menu.ts
@@ -1,16 +1,17 @@
 import { setupMessagingServices } from "../../exports/messages.js";
+import { menuCaption } from "../handle/index.js";
 //
 
 
 
-export async function menu(faputa: any, from: string, m: any) {
+export async function menu(faputa: any, from: string, m: string) {
     const { enviarTexto, enviarAudioGravacao, enviarImagem } = setupMessagingServices(faputa, from, m);
 
     
     try{
-        await enviarAudioGravacao("assets/audios/mmmp3")
-    await enviarImagem("assets/imgs/neko.png")  
-
+        await enviarAudioGravacao("./db/assets/audio/mm.mp3")
+    await enviarImagem("db/assets/img/neko.png",menuCaption(m))  
+    
     } catch (error){
      console.log("errror")
     }
