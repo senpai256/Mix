becfa08a1c238a7712d5b7118dc9c8b7fe488fa0
Jeredy <brunojessie144@gmail.com>
2025-09-19 00:41:51 -0300

    up

--- src/index.ts
+++ src/index.ts
@@ -5,23 +5,12 @@ import { exec, execSync } from 'child_process';
 
 
 async function pico (){
-    const authFolder = "db/qr-code"
-    const resposta = await question("oq vc quer fazer? (1)logar (2) novo login: ")
-    if (resposta === "1") {
-        exec("yarn start")
-        reng()
-    }
-    if (resposta === "2") {
-        if (fs.existsSync(authFolder)){
-            fs.rmdirSync(authFolder, {recursive: true})
-            console.log("pasta de autenticação deletada")
-            
-        }
+   
         reng()
        
     }
 
     
     console.log("pico")
-}
+
 pico()
